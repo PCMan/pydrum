c11334f899f553490aa4baaaddbc39b068f583d7
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2015-12-24 00:40:50 +0800

    Add drum kits taken from Hydrogen project.

---
+++ drumkits/convert.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b\0")
+for f in `find . -name "*.flac" -print`
+do
+ogg=`echo "$f" | sed -e s/flac/ogg/`
+echo "$f to $ogg"
+ffmpeg -y -i $f $ogg
+rm $f
+done
+
+for f in `find . -name '*.wav' -print`
+do
+ogg=`echo "$f" | sed -e s/wav/ogg/`
+echo "$f to $ogg"
+ffmpeg -y -i $f $ogg
+rm $f
+done
+
+
+IFS=$SAVEIFS
